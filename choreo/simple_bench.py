020834d4efac7f05e00178efa12fac4d50050ae2
Vibhatha Lakmal Abeykoon <vibhatha@users.noreply.github.com>
2025-12-07 09:09:59 +0530

    feat: simple http server (#6)

---
+++ choreo/simple_bench.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+import os
+
+PORT = int(os.environ.get("PORT", 8080))
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at port {PORT}")
+        httpd.serve_forever()
